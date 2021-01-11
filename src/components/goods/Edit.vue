dac52a844fce8839dc036ab1811cdab613cc71e5
Tionder <czpk88888888@outlook.com>
2021-01-11 18:16:41 +0800

    完成了商品功能开发

---
+++ src/components/goods/Edit.vue
@@ -0,0 +1,226 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>编辑</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <!-- 提示区域 -->
+      <el-alert title="编辑商品信息" type="info" show-icon :closable="false"></el-alert>
+
+      <!-- 步骤条区域 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+
+      <!-- tab栏区域 -->
+      <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="100px" label-position="top">
+        <el-tabs v-model="activeIndex" :tab-position="'left'" @tab-click="tabClicked">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="editForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="editForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="editForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="editForm.goods_number" type="number"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+            <!-- 渲染表单的Item项 -->
+            <el-form-item :label="item.attr_name" v-for="item in manyTabData" :key="item.attr_id">
+              <!-- 复选框组 -->
+              <el-checkbox-group v-model="item.attr_vals">
+                <el-checkbox :label="cb" v-for="(cb,i) in item.attr_vals" :key="i" border></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+            <el-form-item :label="item.attr_name" v-for="item in onlyTableData" :key="item.attr_id">
+              <el-input v-model="item.attr_vals"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">
+            <!-- action 表示图片要上传的api路径 -->
+            <el-upload :action="uploadUlr" :on-preview="handlePreview" :on-remove="handleRemove" list-type="picture" :headers="headersObj" :on-success="handleSuccess">
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="editForm.goods_introduce"></quill-editor>
+            <!-- 添加商品按钮 -->
+            <el-button type="primary" class="btnEdit" @click="edit">编辑商品</el-button>
+          </el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
+
+    <!-- 图片预览对话框 -->
+    <el-dialog title="图片预览" :visible.sync="previewVisible" width="50%">
+      <img :src="previewPath" class="previewImg" />
+    </el-dialog>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: '0',
+      previewVisible: false,
+      editForm: {
+        goods_name: '',
+        goods_price: '',
+        goods_number: '',
+        goods_weight: '',
+      },
+      editFormRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' },
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价格', trigger: 'blur' },
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' },
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' },
+        ],
+      },
+      manyTabData: [],
+      onlyTableData: [],
+      previewPath: '',
+      uploadUlr: 'http://127.0.0.1:8888/api/private/v1/upload',
+      // 图片上传组件的headers请求头对象
+      headersObj: {
+        Authorization: window.sessionStorage.getItem('token'),
+      },
+    }
+  },
+
+  created() {
+    this.getGoodsById()
+  },
+  methods: {
+    async getGoodsById() {
+      const { data: res } = await this.$http.get(`goods/${this.cateId}`)
+      this.editForm = res.data
+    },
+    async tabClicked() {
+      if (this.activeIndex === '1') {
+        const { data: res } = await this.$http.get(
+          `categories/${this.cateId}/attributes`,
+          {
+            params: { sel: 'many' },
+          }
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取动态参数列表失败!')
+        }
+        res.data.forEach((item) => {
+          item.attr_vals =
+            item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+        })
+        this.manyTabData = res.data
+      } else if (this.activeIndex === '2') {
+        const { data: res } = await this.$http.get(
+          `categories/${this.cateId}/attributes`,
+          {
+            params: { sel: 'only' },
+          }
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取静态属性失败!')
+        }
+        this.onlyTableData = res.data
+      }
+    },
+    handlePreview(file) {
+      this.previewPath = file.response.data.url
+      this.previewVisible = true
+    },
+    edit() {
+      this.$refs.editFormRef.validate(async (valid) => {
+        if (!valid) {
+          return this.$message.error('请填写必要表单项!')
+        }
+        // 处理动态参数和静态属性
+        this.manyTabData.forEach((item) => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals.join(' '),
+          }
+          this.editForm.attrs.push(newInfo)
+        })
+        this.onlyTableData.forEach((item) => {
+          const newInfo = {
+            attr_id: item.attr_id,
+            attr_value: item.attr_vals,
+          }
+          this.editForm.attrs.push(newInfo)
+        })
+
+        const { data: res } = await this.$http.put(
+          `goods/${this.cateId - 0}`,
+          this.editForm
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('编辑商品失败!')
+        }
+        this.$message.success('编辑商品成功!')
+        this.$router.push('/goods')
+      })
+    },
+    handleRemove(file) {
+      // 获取将要移除图片的临时路径
+      const filePath = file.response.data.tmp_path
+      // 从 pics 数组中找到这个图片的索引值
+      const i = this.editForm.pics.findIndex((x) => x.pic === filePath)
+      // 调用数组的 splice 方法，把图片信息对象从pics数组中移除
+      this.editForm.pics.splice(i, 1)
+    },
+    handleSuccess(response) {
+      // 拼接得到一个图片信息对象
+      const picInfo = { pic: response.data.tmp_path }
+      // 将图片信息对象 push 到pics数组中
+      this.editForm.pics.push(picInfo)
+    },
+  },
+  computed: {
+    cateId() {
+      return window.sessionStorage.getItem('goods_id')
+    },
+  },
+}
+</script>
+
+
+<style lang="less" scoped>
+.el-checkbox {
+  margin: 0 10px 0 0 !important;
+}
+
+.previewImg {
+  width: 100%;
+}
+
+.btnEdit {
+  margin-top: 15px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import GoodsList from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
+import Edit from '../components/goods/Edit.vue'
 
 
 Vue.use(VueRouter)
@@ -27,7 +30,10 @@ const router = new VueRouter({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
+        { path: '/goods/edit', component: Edit }
       ]
     }
 
